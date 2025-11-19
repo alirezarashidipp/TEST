afe844b177c5e793e302acff9a0dd55bc3c921c3
Ali Reza Rashidi <141040907+alirezarashidipp@users.noreply.github.com>
2025-11-19 10:17:01 +0100

    Add data cleaning function and update mappings

--- module.py
+++ module.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+def clean_df(df, cols):
+    for c in cols:
+        df[c] = df[c].astype(str).str.strip().str.lower()
+    return df
+
 def load_mapping(mapping_path, mapping_cols):
     mapping = pd.read_excel(mapping_path)
+    mapping = clean_df(mapping, mapping_cols + ["TECH_FLAG"])
     mapping["SPEC"] = mapping[mapping_cols].ne("*").sum(axis=1)
     return mapping.sort_values("SPEC", ascending=False)
 
@@ -10,19 +16,22 @@ def classify_row(row, mapping, mapping_cols, employee_cols):
         if all(rule[m] == "*" or rule[m] == row[e]
                for m, e in zip(mapping_cols, employee_cols)):
             return rule["TECH_FLAG"]
-    return "Unknown"
+    return "unknown"
 
 def apply_mapping(df, mapping_path="mapping.xlsx",
-                  mapping_cols=["JOB_FAMILY","JOB_SUB_FAMILY","JOB_CATEGORY"],
+                  mapping_cols=["JOB_FAMILY", "JOB_SUB_FAMILY", "JOB_CATEGORY"],
                   employee_cols=["FAMILY_NAME","SUB_FAM","CATEGORY_NAME"],
                   new_col="TECH_FLAG"):
-    
+
+    df = clean_df(df, employee_cols)
     mapping = load_mapping(mapping_path, mapping_cols)
+
     df[new_col] = df.apply(
         lambda row: classify_row(row, mapping, mapping_cols, employee_cols),
         axis=1
     )
     return df
+
 
 
 
